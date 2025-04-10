8d75977ae87b5c10f4856af5b6f65e1961a0f809
Carl <carlst99@outlook.co.nz>
2025-04-10 23:09:08 +1200

    Rename package

--- src-zig/build.zig
+++ src-zig/build.zig
@@ -33,7 +33,7 @@ pub fn build(b: *std.Build) void {
     // for actually invoking the compiler.
     const lib = b.addLibrary(.{
         .linkage = .static,
-        .name = "binary_primitive_interactions",
+        .name = "binary_primitive_helpers",
         .root_module = lib_mod,
     });
 
